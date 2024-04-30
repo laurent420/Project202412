19572a613d4cb573209ded66709d084174b112a1
ThomasDN003 <thomas.de.neef@student.ehb.be>
2024-04-30 16:00:55 +0200

    info begin tekst

--- project12/resources/views/Info.blade.php
+++ project12/resources/views/Info.blade.php
@@ -12,7 +12,26 @@
                 {{ __('Info') }}
             </h2>
         </x-slot>
-        
+        <h2>Opening hours</h2>
+        <h3>MediaLab & FabLab</h3>
+<p>Closed on holidays</p>
+<p> Summer closure 2024: 11/07 to 15/08</p>
+
+Monday: 10:00 - 12:00 & 12:30 - 17:00
+Tuesday: closed
+Wednesday: closed
+Thursday: 10:00 - 12:00 & 12:30 - 17:00
+Friday: 10:00 - 12:00 & 12:30 - 17:00
+
+Lending service
+Monday: 10:00 - 12:00 & 12:30 - 17:00
+Tuesday: closed
+Wednesday: closed
+Thursday: closed
+Friday: 10:00 - 12:00 - 12:30 - 17:00
+
+Contact
+gwendo@ehb.be
     </x-app-layout>
 
     
