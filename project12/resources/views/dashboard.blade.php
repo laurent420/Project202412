415f9154f620e6068ec426029f7becb9a1fc46e1
ediered <edenvuako@gmail.com>
2024-05-07 10:02:05 +0200

    test

--- project12/resources/views/dashboard.blade.php
+++ project12/resources/views/dashboard.blade.php
@@ -5,20 +5,6 @@
         </h2>
     </x-slot>
 
-<<<<<<< HEAD
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                    <form action="/search" method="post">
-    @csrf
-    <input type="text" name="query" placeholder="Search">
-    <input type="submit" value="Search">
-</form>
-
-                </div>
-=======
     <div class="container mx-auto">
         <h2 class="text-2xl font-semibold mb-4">Items</h2>
 
@@ -26,7 +12,6 @@
         @if (auth()->check() && auth()->user()->isAdmin())
             <div class="mb-4">
                 <a href="{{ route('additem') }}" class="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">Add Item</a>
->>>>>>> main
             </div>
         @endif
 
@@ -45,4 +30,4 @@
 </div>
     
     </div>
-</x-app-layout> 
+</x-app-layout>
