42a27f2173108672ebd023c17471a3a2ebc71da9
Ahmad-Khn <khan.mohammad@student.ehb.be>
2024-05-15 16:21:43 +0200

    favourites1
    plus heart foto voor favorieten

--- project12/resources/views/Favourites.blade.php
+++ project12/resources/views/Favourites.blade.php
@@ -1,23 +1,33 @@
+<link rel="stylesheet" href="/css/favourites.css">
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Favorites') }}
+            {{ __('Favourites') }}
         </h2>
     </x-slot>
 
     <div class="container mx-auto">
-        <h2 class="text-2xl font-semibold mb-4">Favorite Items</h2>
-
-        <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        @foreach ($favorites as $favorite)
+    @foreach ($favorites as $favorite)
     @if ($favorite->item)
-        <div class="bg-gray-100 p-4 rounded-lg">
-            <h3 class="text-lg font-semibold mb-2">{{ $favorite->item->name }}</h3>
-            <!-- Display other item details -->
+        <div class="py-12">
+         
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+   
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                                    
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        <h3 class="text-lg font-semibold mb-2 ">{{ $favorite->item->name }}</h3>
+                        <div class="w-full flex-none text-sm font-medium text-slate-700 mt-2">
+                            amount left:  
+                        </div>
+                        <button class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mb-2">Add to Bag</button>
+                        <!-- Display other item details -->
+                    </div>
+                </div>     
+         
+            </div>
         </div>
-    @endif
-@endforeach
-
-        </div>
+        @endif
+        @endforeach
     </div>
 </x-app-layout>
