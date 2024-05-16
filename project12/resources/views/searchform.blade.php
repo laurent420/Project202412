408e40fcede06c4522f380473d80423cea3186b0
ediered <edenvuako@gmail.com>
2024-05-16 11:31:08 +0200

    Update searchform.blade.php

--- project12/resources/views/searchform.blade.php
+++ project12/resources/views/searchform.blade.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Search</title>
-</head>
-<body>
-    <form action="{{ route('search') }}" method="GET">
-        <input type="text" name="query" required>
-        <button type="submit">Search</button>
-    </form>
-</body>
+<!-- resources/views/searchform.blade.php -->
+
+<form action="{{ route('search') }}" method="GET">
+    <input type="text" name="query" required>
+    <button type="submit">Search</button>
+</form>
+
 </html>
