cdc4fddd4030fba4598f37d503c6a80e83057124
ediered <edenvuako@gmail.com>
2024-05-16 11:50:46 +0200

    Update searchform.blade.php

--- project12/resources/views/searchform.blade.php
+++ project12/resources/views/searchform.blade.php
@@ -1,8 +1,4 @@
-<!-- resources/views/searchform.blade.php -->
-
 <form action="{{ route('search') }}" method="GET">
     <input type="text" name="query" required>
     <button type="submit">Search</button>
 </form>
-
-</html>
