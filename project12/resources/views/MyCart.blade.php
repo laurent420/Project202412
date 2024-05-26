45c3ad74390112d9f3d70e6756d86578aebb1ebb
Mohamed1500 <145970764+Mohamed1500@users.noreply.github.com>
2024-05-26 23:31:12 +0200

    code toegevoegd voor my bag

--- project12/resources/views/MyCart.blade.php
+++ project12/resources/views/MyCart.blade.php
@@ -3,28 +3,44 @@
 
 @else
     <x-app-layout>
-            <x-slot name="header">
-                <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                    {{ __('My Bag') }}
-                </h2>
-            </x-slot>
+        <x-slot name="header">
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                {{ __('My Bag') }}
+            </h2>
+        </x-slot>
 
-            <div class="container mx-auto">
-                <h2 class="text-2xl font-semibold mb-4">My Bag Items</h2>
+        <div class="container mx-auto">
+            
 
-                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                    @foreach ($cartItems as $cartItem)
-                        @if ($cartItem->item)
-                            <div class="bg-gray-100 p-4 rounded-lg">
-                                <h3 class="text-lg font-semibold mb-2">{{ $cartItem->item->name }}</h3>
-                                <p>Quantity: {{ $cartItem->quantity }}</p>
-                                <button class="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 mb-2 remove-from-cart" data-cart-item-id="{{ $cartItem->id }}">
-                                    Remove from Bag
-                                </button>
-                            </div>
-                        @endif
-                    @endforeach
-                </div>
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                @php
+                    $bagIsEmpty = true;
+                @endphp
+
+                @foreach ($cartItems as $cartItem)
+                    @if ($cartItem->item)
+                        @php
+                            $bagIsEmpty = false;
+                        @endphp
+                        <div class="bg-gray-100 p-4 rounded-lg">
+                            <h3 class="text-lg font-semibold mb-2">{{ $cartItem->item->name }}</h3>
+                            <p>Quantity: {{ $cartItem->quantity }}</p>
+                            <button class="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 mb-2 remove-from-cart" data-cart-item-id="{{ $cartItem->id }}">
+                                Remove from Bag
+                            </button>
+                        </div>
+                    @endif
+                @endforeach
+
+                
+                @if ($bagIsEmpty)
+            
+                <div class="font-semibold mb-2 text-center text-4xl">Nothing in your bag</h3>
+    </div>
+
+                @endif
+
             </div>
-        </x-app-layout>
+        </div>
+    </x-app-layout>
 @endif
