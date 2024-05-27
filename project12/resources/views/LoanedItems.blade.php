7cb255cb6a79ebd3a4ebb5a7d644ac652354fb30
laurent420 <laurent.pladet@student.ehb.be>
2024-05-27 10:02:20 +0200

    Update LoanedItems.blade.php

--- project12/resources/views/LoanedItems.blade.php
+++ project12/resources/views/LoanedItems.blade.php
@@ -1,5 +1,9 @@
-@if ( auth()->user()->is_banned  == 1)
-    <h1>You are banned</h1>    
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Loaned Items') }}
+        </h2>
+    </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
